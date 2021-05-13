57a0fde71ada68e53232e46cc6f220f7fd18083b
akantifirst <82590770+akantifirst@users.noreply.github.com>
2021-05-13 13:59:16 +0200

    Update bot.py
    Changed answers to html mode, refined /start and /help commands

--- bot.py
+++ bot.py
@@ -4,13 +4,25 @@ from calc import calc
 bot = telebot.TeleBot('1779223371:AAFDFO38aEeVMCZzT4tSe1VczuFk4y-OCB4')
 
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(message.chat.id,
                      'Welcome to power/current converter. \n'
-                     'Please enter either current (add suffix A) or power (add suffix kW),\n'
-                     'and optionally voltage and power factor.\n'
-                     'Type /help for some examples')
+                     'Please enter either current (add suffix A) or power (add suffix kW).\n'
+                     'Optionally you can enter voltage (add suffix V).\n'
+                     'and power factor (a number from 0.5 to 1).\n\n'
+                     'Example:\n    <i>12kw 230v,\n    20a 0.8,\n    34kw 400V 0.95</i>\n'
+                     'Type /help for more examples.', parse_mode='HTML')
+
+
+@bot.message_handler(commands=['help'])
+def start_command(message):
+    bot.send_message(message.chat.id,
+                     '<b>12kw  => </b> <i>12kW 400V cos(φ)=0.95</i>\n'
+                     '<b>12kW 0.8  => </b> <i>12kW 400V cos(φ)=0.8</i>\n'
+                     '<b>12kW 230V 0.9  => </b> <i>12kW 230V cos(φ)=0.9</i>\n'
+                     '<b>35a 230V 0.6  => </b> <i>35A 230V cos(φ)=0.6</i>\n'
+                     '<b>12  => </b> <i>12kW 400V cos(φ)=0.95</i>', parse_mode='HTML')
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
@@ -18,10 +30,10 @@ def start_command(message):
 def request_message(message):
     try:
         power, current, voltage, power_factor = calc(message.text)
-        bot.reply_to(message, f'P = {power:.1f} kW, \n'
-                              f'I = {current:.1f} A, \n'
-                              f'U =  {voltage:.0f} V, \n'
-                              f'cos(φ) = {power_factor:.2f}')
+        bot.reply_to(message, f'P = <b>{power:.1f}</b> kW, \n'
+                              f'I = <b>{current:.1f}</b> A, \n'
+                              f'<i>U = {voltage:.0f} V, \n'
+                              f'cos(φ) = {power_factor:.2f}</i>', parse_mode='HTML')
     except TypeError:
         bot.reply_to(message, 'Please, review your request')
 
