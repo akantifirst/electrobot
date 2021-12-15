6b83e0bf7ac71dec21b3810f9001e8dcefbe6aa9
Alexander Popov <akanti@ya.ru>
2021-12-15 18:38:57 +0000

    New version of the bot. Added dU, cable section, circuit breaker, cable material, switchboard name, laying and many other parameters

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from config import token
-from calc import calc
+import asyncio
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.types import Message
+import calc
+from config import TOKEN
 
-bot = Bot(token=token)
-dp = Dispatcher(bot)
+form_router = Router()
 
 
-@dp.message_handler(commands=['start'])
+@form_router.message(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply('Welcome to power/current converter. \n'
                         'Please enter either current (add suffix A) or power (add suffix kW).\n'
@@ -18,7 +17,7 @@ async def process_start_command(message: types.Message):
                         'Type /help for more examples.', parse_mode='HTML')
 
 
-@dp.message_handler(commands=['help'])
+@form_router.message(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply('<b>12kw  => </b> <i>12kW 400V cos(φ)=0.95</i>\n'
                         '<b>12kW 0.8  => </b> <i>12kW 400V cos(φ)=0.8</i>\n'
@@ -27,18 +26,50 @@ async def process_help_command(message: types.Message):
                         '<b>12  => </b> <i>12kW 400V cos(φ)=0.95</i>', parse_mode='HTML')
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    # Handling incorrect user input
+@form_router.message(commands=['laying'])
+async def process_help_command(message: types.Message):
+    await message.reply('<b>A1</b> <i> - in Installationsrohren in wärmegedämmten Wänden</i>\n'
+                        '<b>A2</b> <i> - in Installationsrohren in wärmegedämmten Wänden; Mehradrig</i>\n'
+                        '<b>B1</b> <i> - in Installationsrohren auf einer Wand</i>\n'
+                        '<b>B2</b> <i> - in Installationsrohren auf einer Wand; Mehradrig</i>\n'
+                        '<b>C1</b> <i> - direkt auf einer Wand</i>\n'
+                        '<b>D1</b> <i> - in Erde</i>\n'
+                        '<b>E1</b> <i> - Installationskanal</i>\n'
+                        '<b>F1</b> <i> - auf gelochter Kabelwanne mit Berührung; Einadrig</i>\n'
+                        '<b>F2</b> <i> - auf gel. Kabelwanne mit Berührung; Eine Schicht; Mehradrig</i>\n'
+                        '<b>F3</b> <i> - auf gel. Kabelwanne mit Berührung; Mehrere Schichte; Mehradrig</i>\n'
+                        '<b>G1</b> <i> - auf gel. Kabelwanne ohne Berührung; Horizontal</i>\n'
+                        '<b>G2</b> <i> - auf gel. Kabelwanne ohne Berührung; Vertikal</i>\n', parse_mode='HTML')
+
+
+@form_router.message()
+async def echo_message(message: Message):
     try:
-        power, current, voltage, power_factor = calc(msg.text)
-        await bot.send_message(msg.from_user.id, f'P = <b>{power:.1f}</b> kW, \n'
-                                                 f'I = <b>{current:.1f}</b> A, \n'
-                                                 f'<i>U = {voltage:.0f} V, \n'
-                                                 f'cos(φ) = {power_factor:.2f}</i>', parse_mode='HTML')
+        parsed_data = calc.parse_input(message.text)
+        computed_data = calc.calc(parsed_data)
+        fdata = calc.format_values(computed_data)
+
+        await message.answer(f'<code>Verteiler:     {fdata[0]}\n'
+                             f'Leistung:      {fdata[1]}\n'
+                             f'Strom:         {fdata[2]}\n'
+                             f'Spannung:      {fdata[3]}\n'
+                             f'cos(φ):        {fdata[4]}\n'
+                             f'Kabel:         {fdata[7]} {fdata[6]}({fdata[8]}) {fdata[11]}\n'
+                             f'Verlegeart:    {fdata[5]}\n'
+                             f'Schutzorgan:   {fdata[14]}\n'
+                             f'Spannungsfall: {fdata[13]}</code>',
+                             parse_mode='HTML')
     except (TypeError, ValueError):
-        await msg.reply('Please, review your request')
+        await message.reply('Please, review your request [bot]')
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+async def main():
+    bot = Bot(token=TOKEN, parse_mode="HTML")
+    dp = Dispatcher()
+    dp.include_router(form_router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
-import math
-from contextlib import suppress
+import csv
+from math import sqrt, sin, acos
+import random
+import string
 
 
 def clear_chars(input_string):
@@ -11,55 +13,207 @@ def clear_chars(input_string):
     return number
 
 
-def calc(message):
+def parse_input(user_input):
+    """
+    This function parses user input and finds the values of
+    electrical current, power, power factor or voltage
+    input:
+    uv-av-o1 35kw 380 0.98 d1 nycwy 56m	*alu *1.OGg *abb
+    output:
+    [ name,      P,  I,    U,   phi,  laying, cable, length, medium, floor,  maker, du_max]
+	["uv-av-eg", 35, None, 400, 0.95, "c1",   "nyy", 120,    "alu",  "1.og", "abb", 2.5]
+	"""
+    e = "All good!"  # for debugging
+    # These are default values todo move to config
+    name = 'UV-AV-XX'
+    power = ' '
+    current = ' '
+    voltage = 400
+    phi = 0.95
+    laying = "e1"
+    cable = "nyy"
+    length = 30
+    medium = "cu"
+    floor = ' '
+    maker = "abb"
+    du_max = 1.5
+
+    # Refine user input
+    data = user_input.replace(',', '.').lower().split(' ')
+    # get name
+    if sum(c.isalpha() for c in data[0]) > 2 or sum(c.isdigit() for c in data[0]) == 0:
+        name = data[0]
+        data = data[1:]
+
+    try:
+        for e in data:
+            if e[len(e) - 1] == 'a':
+                current = clear_chars(e)  # get current
+            elif any([x in e for x in ['kw']]):
+                power = clear_chars(e)  # get power
+            elif any([x in e for x in ['nyy', 'nym', 'nycwy', 'nhxh']]):
+                cable = str(e)  # get cable
+            elif any([x in e for x in ['v']]):
+                voltage = clear_chars(e)  # get voltage
+            elif any([x in e for x in ['m']]):
+                length = clear_chars(e)  # get length
+            elif any([x in e for x in ['a1', 'a2', 'b1', 'b2', 'c1', 'd1', 'e1', 'f1', 'f2', 'f3', 'g1', 'g2']]):
+                laying = str(e)  # get laying
+            elif any([x in e for x in ['alu', 'cu']]):
+                medium = str(e)  # get medium
+            elif e[len(e) - 1] == 'g':
+                floor = str(e)[:-1]  # get floor
+            elif any([x in e for x in ['abb', 'siemens', 'hager']]):
+                maker = str(e)  # get maker
+            elif e[len(e) - 1] == '%':
+                du_max = float(e[:-1])  # get du_max
+            elif 0.6 <= float(e) <= 1.0:
+                phi = float(e)
+
+        parsed_data = [name, power, current, voltage, phi, laying, cable, length, medium, floor, maker, du_max]
+        return parsed_data
+    except ValueError:
+        print(f'Please review your request: {e} [parse_input]')
+
+
+# parse_input("uv-av-o1 24kw 380 0.98 g1 nycwy 56m alu abb 1,5%")
+
+
+def power_current(power, current, voltage, phi):
     """This function calculates electrical power in kW
-    or electrical current in A"""
-    # Where the user request is processed.
-    power, current, voltage, power_factor = parse_input(message)
+        or electrical current in A"""
     # Not sure if user wants power or current
     try:
-        if voltage >= 380:
-            power = (math.sqrt(3) * power_factor * voltage * current) / 1000
+        if voltage >= 300:
+            power = round((sqrt(3) * phi * voltage * current) / 1000, 2)
         else:
-            power = (power_factor * voltage * current) / 1000
+            power = round((phi * voltage * current) / 1000, 2)
     except TypeError:
         # If this happens, {current} variable still equals whitespace and cant be divided
         # It means, that user entered power and expects current to be calculated
-        if voltage >= 380:
-            current = 1000 * power / (math.sqrt(3) * power_factor * voltage)
+        if voltage >= 300:
+            current = round(1000 * power / (sqrt(3) * phi * voltage), 2)
         else:
-            current = 1000 * power / (power_factor * voltage)
-    return power, current, voltage, power_factor
+            current = round(1000 * power / (phi * voltage), 2)
+    return power, current
 
 
-def parse_input(user_input):
-    """This function parses user input and finds the values of
-    electrical current, power, power factor or voltage"""
-    # These are default values
-    power, current, voltage, power_factor = ' ', ' ', 400, 0.95
-    # Refine user input
-    print(user_input)
-    data = user_input.replace(',', '.').lower().split(' ')
-    # First check if user provided suffices to values
+def cable_section(voltage, length, current, phi, medium, laying, du_max):
+    """
+    Finds in table the minimal cable section allowed for this current and kind of laying
+    """
+    data, section_ib, section_du, system_ib, system_du, system = [], '?', '?', 1, 1, 1
+    laying = laying + '.3' if voltage >= 370 else laying + '.2'
+    for row in csv.reader(open(r'/nb_electrobot/CU_Z70_U30.csv', 'r'), delimiter=';'):
+        data.append(row)
+    # find the right column based on the way of laying:
+    index_laying = data[0].index(laying)
+    current_list = [e[index_laying] for e in data][1:]
+    section_list = [e[0] for e in data][1:]
+    # check current condition:
+    for system_ib in range(1, 8):
+        try:
+            section_ib = next(data[i + 1][0] for i, v in enumerate(current_list) if float(v) >= current / system_ib)
+            break
+        except (ValueError, IndexError):
+            print(r'Cant find cable section - check index or value')
+        except StopIteration:
+            continue
+    # check du condition
+    ro = 0.0225 if medium == 'cu' else 0.036
+    x = 0.08
+    for system_du in range(1, 4):
+        try:
+            section_du = (ro * length * phi) / \
+                 ((voltage * du_max / (sqrt(3) * (current/system_du) * 100)) - (x * length * sin(acos(phi)) / 1000))
+            section_du = next(v for v in section_list if float(v) >= section_du)
+            break
+        except StopIteration:
+            continue
     try:
-        for e in data:
-            if any([x in e for x in ['a', 'а']]):
-                current = clear_chars(e)
-            elif any([x in e for x in ['kw', 'кв']]):
-                power = clear_chars(e)
-            elif any([x in e for x in ['v', 'в']]):
-                voltage = clear_chars(e)
-            else:
-                # If suffices are missing, try to guess what user means
-                with suppress(IndexError):
-                    # Check if first element of input is a number
-                    if not data[0].islower():
-                        power = float(data[0])
-                    if 0.5 <= float(e) <= 1:
-                        power_factor = float(e)
-                    # If no suffix vor voltage specifies, choose between 230V and 400V
-                    if e in {'220', '230', '400'}:
-                        voltage = float(e)
-        return power, current, voltage, power_factor
+        if float(section_ib*system_ib) > float(section_du*system_du):
+            section = float(section_ib)
+            system = system_ib
+        else:
+            section = float(section_du)
+            system = system_du
+
+        du = round(100 * (sqrt(3) * ((ro * length * phi) / float(section) +
+                                     (x * length * sin(acos(phi))) / 1000) * (current/system)) / voltage, 2)
     except ValueError:
-        print('Please review your request')
+        section, du = '?', '?'
+    return section, system, du
+
+
+def circuit_breaker(current):
+    data = []
+    for row in csv.reader(open(r'/nb_electrobot/CB.csv', 'r'), delimiter=';'):
+        data.append(row)
+    cb_currents = [e[0] for e in data[1:]]
+    cb_current = next(v for v in cb_currents if float(v) >= current)
+    current = "LS-Schalter " + str(cb_current) + "A"
+    return current
+
+
+def calc(parsed_data):
+    # Where the user request is processed.
+    name, power, current, voltage, phi, laying, cable, length, medium, floor, maker, du_max = parsed_data
+    # calculate power and current
+    power, current = power_current(power, current, voltage, phi)
+    # now that we have current we can calculate the cable section
+    section, system, du = cable_section(voltage, length, current, phi, medium, laying, du_max)
+
+    cb = circuit_breaker(current)
+    computed_data = [name, power, current, voltage, phi, laying,
+                     cable, length, medium, floor, maker, section, system, du, cb]
+    return computed_data
+
+
+# calc(['uv-av-o1', 300.0, ' ', 400, 0.98, 'c1', 'nycwy', 56.0, 'alu', '1.og', 'abb', 1.5])
+
+
+def format_values(feeder):
+    """
+    This function formats all computed values for human interaction
+    either in form of a pdf file or by message in Telegram
+    """
+    poles = "5" if feeder[3] > 240 else "3"
+    system = str(feeder[12]) + "x" if feeder[12] > 1 else ''
+    feeder[0] = feeder[0].upper()
+    # feeder power
+    feeder[1] = (f'{float(feeder[1]):.1f}' + "kW").replace('.', ',')
+    # feeder current
+    feeder[2] = (f'{float(feeder[2]):.1f}' + "A").replace('.', ',')
+    # feeder voltage
+    feeder[3] = f'{float(feeder[3]):.0f}' + "V"
+    # feeder phi
+    feeder[4] = str(feeder[4]).replace('.', ',')
+    # feeder_laying
+    try:
+        data = []
+        for row in csv.reader(open(r'/nb_electrobot/LAYING.csv', 'r', encoding='utf8'), delimiter=';'):
+            data.append(row)
+        tdata = list(zip(*data))
+        # find the right column based on the way of laying:
+        index_laying = list(tdata[0]).index(feeder[5][:2].lower())
+        feeder[5] = list(tdata[1])[index_laying]
+    except (TypeError, ValueError):
+        print('Please review your request [format_values.laying]')
+    # feeder cable
+    feeder[6] = feeder[6].upper()
+    # feeder length
+    feeder[7] = f'{float(feeder[7]):.0f}' + "m"
+    # feeder medium
+    feeder[8] = feeder[8].capitalize()
+    # feeder floor
+    feeder[9] = feeder[9].upper()
+    # feeder maker
+    feeder[10] = feeder[10].upper()
+    # feeder section
+    feeder[11] = system + (poles + "x" + str(feeder[11]).rstrip('0').rstrip('.')).replace('.', ',') + "mm²"
+
+    # feeder voltage drop
+    feeder[13] = (str(feeder[13]) + "%").replace('.', ',')
+    # feeder circuit breaker
+    feeder[14] = feeder[14].replace('.', ',')
+    return feeder
